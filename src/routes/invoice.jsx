61391a6db20c3f2f27b602de92023666f959a711
asterfinde <dante.frias@gmail.com>
2021-11-11 10:46:01 -0500

    1 file modified

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,33 +1,32 @@
-import { useParams, useNavigate } from "react-router-dom";
-import { getInvoice, deleteInvoice } from "../data";
+/**
+ * From: '<Route path=":invoiceId" element={<Invoice />} />' in 'main.jsx'
+ *        where the routing is defined to the corresponding components
+ * To  : 'params.invoiceId'
+ *
+ */
+
+import { useParams, useNavigate } from 'react-router-dom';
+import { getInvoice, deleteInvoice } from '../data';
 
 export default function Invoice() {
   let navigate = useNavigate();
   let params = useParams();
-
-  // From: '<Route path=":invoiceId" element={<Invoice />} />' in main.jsx
-  // where routing to corresponding components
-  // To: 'params.invoiceId'
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
 
   const onClickDeleteInvoice = () => {
     deleteInvoice(invoice.number);
-    navigate("/invoices");
-  }
+    navigate('/invoices');
+  };
 
   return (
-    <main style={{ padding: "1rem" }}>
+    <main style={{ padding: '1rem' }}>
       <h2>Total Due: {invoice.amount}</h2>
       <p>
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
       <p>
-        <button
-          onClick={ onClickDeleteInvoice }
-        >
-          Delete
-        </button>
+        <button onClick={onClickDeleteInvoice}>Delete</button>
       </p>
     </main>
   );
